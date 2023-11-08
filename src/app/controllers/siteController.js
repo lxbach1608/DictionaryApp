06312e28382611d2be68aa2cb16fcf6260062b2b
bale <lxbach1608@gmail.com>
2023-11-08 23:24:15 +0700

    render mongoose to hbs, add util folder contains mongoose helper

--- src/app/controllers/siteController.js
+++ src/app/controllers/siteController.js
@@ -1,11 +1,21 @@
 const Word = require('../models/Word');
+const { mongoosesToObjects, mongooseToObject } = require('../util/mongoose');
 
 class siteController {
   // [GET] path: "/"
-  index(req, res) {
-    res.json({
-      value: 'test',
-    });
+  async index(req, res) {
+    let instance;
+    try {
+      instance = await Word.find({});
+
+      const words = instance;
+
+      res.render('home', { words: mongoosesToObjects(words) });
+    } catch (err) {
+      res.status(400).json({ error: "Error: Can't find word" });
+
+      return;
+    }
   }
 }
 
